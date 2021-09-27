124484091aa111902ad6e3ae5b669050e469acfa
fayeed <fayeed@live.com>
2021-09-27 11:31:09 +0530

    feat(video) - Project setup

---
+++ packages/audio-video-view/src/components/app.tsx
@@ -0,0 +1,12 @@
+import { FunctionalComponent, h } from 'preact';
+
+
+const App: FunctionalComponent = () => {
+    return (
+        <div id="preact_root">
+            <div>hello</div>
+        </div>
+    );
+};
+
+export default App;
