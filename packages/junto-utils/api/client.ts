21c08a32fcfce8bb7db3bafcdc6fe70c735875cf
fayeed <fayeed@live.com>
2022-02-14 08:16:23 +0530

    feat(chat) - Dynamic port for perspective view

--- packages/junto-utils/api/client.ts
+++ packages/junto-utils/api/client.ts
@@ -2,9 +2,11 @@ import { Ad4mClient } from "@perspect3vism/ad4m";
 import { ApolloClient, ApolloLink, InMemoryCache } from "@apollo/client";
 import { WebSocketLink } from "@apollo/client/link/ws";
 
+const PORT = parseInt(window.location.search.slice(6))
+
 const apolloClient = new ApolloClient({
   link: new WebSocketLink({
-    uri: `ws://localhost:4000/graphql`,
+    uri: `ws://localhost:${PORT}/graphql`,
     options: {
       reconnect: true,
     },
