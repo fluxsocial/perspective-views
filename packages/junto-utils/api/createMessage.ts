ebcb7cea40039abe10a15ee4ef5a7df4fa387d9b
fayeed <fayeed@live.com>
2022-02-16 11:58:06 +0530

    fix(chat) - Removed unused import

--- packages/junto-utils/api/createMessage.ts
+++ packages/junto-utils/api/createMessage.ts
@@ -1,5 +1,5 @@
 import ad4mClient from "./client";
-import { LinkQuery, Link } from "@perspect3vism/ad4m";
+import { Link } from "@perspect3vism/ad4m";
 
 export interface Payload {
   perspectiveUuid: string;
