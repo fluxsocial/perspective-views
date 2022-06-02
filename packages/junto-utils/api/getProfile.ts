ac89059f9587ffa0985f98d6428cdd5e7976800b
Leif Riksheim <riksheim.leif@gmail.com>
2022-06-02 11:59:08 +0200

    chore: add predicate constants

--- packages/junto-utils/api/getProfile.ts
+++ packages/junto-utils/api/getProfile.ts
@@ -1,71 +1,73 @@
 import { Profile } from "../types";
 import ad4mClient from "../api/client";
 import { LinkExpression } from "@perspect3vism/ad4m";
+import {
+  USERNAME,
+  GIVEN_NAME,
+  FAMILY_NAME,
+  EMAIL,
+  PROFILE_IMAGE,
+  PROFILE_THUMBNNAIL_IMAGE,
+  FLUX_PROFILE,
+} from "../constants/profile";
 
 export interface Payload {
   url: string;
-  perspectiveUuid: string
+  perspectiveUuid: string;
 }
 
 export default async function getProfile(url: string): Promise<Profile | null> {
-  const did =  url.includes('://') ? url.split('://')[1] : url;
+  const did = url.includes("://") ? url.split("://")[1] : url;
 
   const agentPerspective = await ad4mClient.agent.byDID(did);
 
   const agentLinks = agentPerspective!.perspective!.links;
 
-  const profile: Profile = {
+  let profile: Profile = {
     did,
     url,
     author: did,
-    username: '',
-    email: '',
-    givenName: '',
-    familyName: '',
+    username: "",
+    email: "",
+    givenName: "",
+    familyName: "",
     timestamp: new Date().toISOString(),
-    thumbnailPicture: '',
-    profilePicture: ''
+    thumbnailPicture: "",
+    profilePicture: "",
   };
 
-  for (const link of agentLinks.filter(e => e.data.source === 'flux://profile')) {
-    let expUrl;
-    let image;
+  const profileLinks = agentLinks.filter((e) => e.data.source === FLUX_PROFILE);
 
-    switch (link.data.predicate) {
-      case "sioc://has_username":
-        profile!.username = link.data.target;
+  for (const { data: target, predicate, source } of profileLinks) {
+    switch (predicate) {
+      case USERNAME:
+        profile!.username = target;
         break;
-      case "sioc://has_given_name":
-        profile!.givenName = link.data.target;
+      case GIVEN_NAME:
+        profile!.givenName = target;
         break;
-      case "sioc://has_family_name":
-        profile!.familyName = link.data.target;
+      case FAMILY_NAME:
+        profile!.familyName = target;
         break;
-      case "sioc://has_profile_image":
-        expUrl = link.data.target;
-        image = await ad4mClient.expression.get(expUrl);
-    
+      case PROFILE_IMAGE:
+        var image = await ad4mClient.expression.get(target);
         if (image) {
           profile!.profilePicture = image.data.slice(1, -1);
         }
         break;
-      case "sioc://has_profile_thumbnail_image":
-        expUrl = link.data.target;
-        image = await ad4mClient.expression.get(expUrl);
-
+      case PROFILE_THUMBNNAIL_IMAGE:
+        var image = await ad4mClient.expression.get(target);
         if (image) {
-          if (link.data.source === "flux://profile") {
-            profile!.thumbnailPicture = image.data.slice(1, -1);
-          }
+          profile!.thumbnailPicture = image.data.slice(1, -1);
         }
         break;
-      case "sioc://has_email":
-        profile!.email = link.data.target;
+      case EMAIL:
+        profile!.email = target;
         break;
       default:
         break;
     }
   }
 
-  return profile
+  return profile;
 }
