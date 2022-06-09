56167aaec2164673c63195e37e735bada9c623ec
Leif Riksheim <riksheim.leif@gmail.com>
2022-06-09 14:21:44 +0200

    chore: for now just get the jwt token from localstorage

--- packages/junto-utils/api/client.ts
+++ packages/junto-utils/api/client.ts
@@ -2,13 +2,20 @@ import { Ad4mClient } from "@perspect3vism/ad4m";
 import { ApolloClient, ApolloLink, InMemoryCache } from "@apollo/client";
 import { WebSocketLink } from "@apollo/client/link/ws";
 
-const PORT = parseInt(window.location.search.slice(6))
+const PORT = 12000;
 
 const apolloClient = new ApolloClient({
   link: new WebSocketLink({
     uri: `ws://localhost:${PORT}/graphql`,
     options: {
       reconnect: true,
+      connectionParams: async () => {
+        return {
+          headers: {
+            authorization: localStorage.getItem("ad4minToken") || "",
+          },
+        };
+      },
     },
   }),
   cache: new InMemoryCache({ resultCaching: false, addTypename: false }),
