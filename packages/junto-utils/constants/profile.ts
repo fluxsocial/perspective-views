21d3bbfc689d02ef680c20af7c8657f4780118d0
Leif Riksheim <riksheim.leif@gmail.com>
2022-06-13 23:32:19 +0200

    chore: get profile correctly

--- packages/junto-utils/constants/profile.ts
+++ packages/junto-utils/constants/profile.ts
@@ -11,3 +11,7 @@ export const PROFILE_THUMBNNAIL_IMAGE = "sioc://has_profile_thumbnail_image";
 export const FLUX_PROFILE = "flux://profile";
 
 export const EMAIL = "sioc://has_email";
+
+export const BIO = "sioc://has_bio";
+
+export const BG_IMAGE = "sioc://has_bg_image";
