7cd9df09087ac2b1f174d86673582f113360404a
jdeepee <joshuadparkin@gmail.com>
2022-07-28 16:44:07 +0100

    use new channel predicate

--- packages/junto-utils/constants/ad4m.ts
+++ packages/junto-utils/constants/ad4m.ts
@@ -4,6 +4,6 @@ export const LANGUAGE = "ad4m://language";
 
 export const EXPRESSION = "sioc://content_of";
 
-export const CHANNEL = "sioc://has_space";
+export const CHANNEL = "flux://has_channel";
 
 export const DIRECTLY_SUCCEEDED_BY = "temp://directly_succeeded_by";
