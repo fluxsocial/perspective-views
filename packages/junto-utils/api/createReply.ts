d8f9c3b4a793dbb49d6271f3cc2c6ab29f959cc2
fayeed <fayeed@live.com>
2022-08-09 16:54:04 +0530

    Create reply from literal

--- packages/junto-utils/api/createReply.ts
+++ packages/junto-utils/api/createReply.ts
@@ -1,37 +1,27 @@
 import { Link } from "@perspect3vism/ad4m";
+import { REPLY_TO } from "../constants/ad4m";
 import ad4mClient from "./client";
 
 export interface Payload {
   perspectiveUuid: string;
-  languageAddress: string;
   replyUrl: string;
   message: Object;
 }
 
 export default async function ({
   perspectiveUuid,
-  languageAddress,
   replyUrl,
   message,
 }: Payload) {
   try {
-    const expUrl = await ad4mClient.expression.create(message, languageAddress);
+    const expUrl = await ad4mClient.expression.create(message, 'literal');
 
     await ad4mClient.perspective.addLink(
       perspectiveUuid,
       new Link({
-        source: "sioc://chatchannel",
+        source: replyUrl,
         target: expUrl,
-        predicate: "sioc://content_of",
-      })
-    );
-
-    await ad4mClient.perspective.addLink(
-      perspectiveUuid,
-      new Link({
-        source: expUrl,
-        target: replyUrl,
-        predicate: "sioc://reply_to",
+        predicate: REPLY_TO,
       })
     );
   } catch (e: any) {
