fbaf9c799e1e2cc0e4fe3fc338dff47d9a09b815
jdeepee <joshuadparkin@gmail.com>
2022-08-25 17:55:47 +0100

    remove old vue code

--- packages/junto-utils/vue/index.ts
+++
@@ -1,5 +0,0 @@
-import useMessages from "./useMessages";
-import useMembers from "./useMembers";
-import usePerspective from "./usePerspective";
-
-export { useMessages, useMembers, usePerspective };
